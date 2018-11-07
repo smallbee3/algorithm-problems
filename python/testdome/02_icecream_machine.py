10e8d36dec853d0116b061f117138960ee2efd06
smallbee <smallbee3@gmail.com>
2018-11-07 22:25:15 +0900

    [python/testdome] 02_icecream_machine solved

---
+++ python/testdome/02_icecream_machine.py
@@ -0,0 +1,52 @@
+
+# 02 Ice Cream Machine
+
+# Implement the IceCreamMachine's scoops method so that it returns all combinations of
+# one ingredient and one topping.
+# If there are no ingredients or toppings, the method should return an empty list.
+# For example, IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"]).scoops()
+# should return [['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']].
+
+
+# (Time: 24 minutes)
+
+# Tests: 4 pass / 0 fail
+#   Example case: Correct answer
+#   Various ingredients and one topping: Correct answer
+#   Various ingredients and toppings: Correct answer
+#   No ingredients and no toppings: Correct answer
+
+
+class IceCreamMachine:
+
+    def __init__(self, ingredients, toppings):
+        self.ingredients = ingredients
+        self.toppings = toppings
+
+    def scoops(self):
+
+        icecream_with_toppings = []
+
+        """
+        Didn't read the question carefully!
+        """
+        # for i in self.ingredients:
+        #     icecream_with_toppings.append([i])
+        #
+        # for j in icecream_with_toppings:
+        #     for k in self.toppings:
+        #         j.append(k)
+
+        """
+        Key point : decide the order of nested for loop (toppings -> ingredients)
+        """
+        for i in self.toppings:
+            for j in self.ingredients:
+                icecream_with_toppings.append([j, i])
+
+        return icecream_with_toppings
+
+
+# icecream = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
+icecream = IceCreamMachine(["vanilla", "chocolate"], ["sauce1", "sauce2", "sauce3"])
+print(icecream.scoops())
