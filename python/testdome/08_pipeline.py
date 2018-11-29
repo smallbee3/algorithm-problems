b35ae1b21f2572c57d74af292b2dc4cbb97f87ab
smallbee <smallbee3@gmail.com>
2018-11-29 23:27:15 +0900

    [python/testdome] 08_pipeline reviewed with 'scope' : UnboundLocalError studied

--- python/testdome/08_pipeline.py
+++ python/testdome/08_pipeline.py
@@ -53,26 +53,25 @@ Tests: 2 pass / 1 fail
 #                 return result
 #         return helper
 
-# class Pipeline:
-#     @staticmethod
-#     def pipeline(*funcs):
-#         i = 0
-#
-#         def helper(arg):
-#             # Step 2
-#             nonlocal i
-#             result = funcs[i](arg)
-#             i += 1
-#             if i < len(funcs):
-#                 return helper(result)
-#             else:
-#                 return result
-#         return helper
-#
-#
-# fun = Pipeline.pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
-# # fun = Pipeline.pipeline(lambda x: x * 3)
-# print(fun(3))  # should print 5.0
+class Pipeline:
+    @staticmethod
+    def pipeline(*funcs):
+        i = 0
+
+        def helper(arg):
+            nonlocal i
+            result = funcs[i](arg)
+            i += 1
+            if i < len(funcs):
+                return helper(result)
+            else:
+                return result
+        return helper
+
+
+fun = Pipeline.pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
+# fun = Pipeline.pipeline(lambda x: x * 3)
+print(fun(3))  # should print 5.0
 
 """
 181111 Review
@@ -85,22 +84,99 @@ Tests: 3 pass / 0 fail
 """
 
 
+# class Pipeline:
+#     @staticmethod
+#     def pipeline(*funcs):
+#         def helper(arg, *func_list):
+#             if not func_list:
+#                 arg = funcs[0](arg)
+#                 func_list = funcs
+#             else:
+#                 func_list = func_list[0]
+#                 arg = func_list[0](arg)
+#
+#             if len(func_list) == 1:
+#                 return arg
+#             return helper(arg, func_list[1:])
+#         return helper
+#
+#
+# fun = Pipeline.pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
+# print(fun(3))  # should print 5.0
+#
+
+"""
+181129 Review
+
+Time : 60 min
+Tests: 3 pass / 0 fail
+  Example case: Correct answer 
+  Various functions: Correct answer 
+  Various data types: Correct answer 
+"""
+
+
+# 1) Various functions: "pipeline.py", line 11, in helper: IndexError
+
 class Pipeline:
     @staticmethod
     def pipeline(*funcs):
-        def helper(arg, *func_list):
-            if not func_list:
-                arg = funcs[0](arg)
-                func_list = funcs
-            else:
-                func_list = func_list[0]
-                arg = func_list[0](arg)
+        def helper(arg, *funcs2):
 
-            if len(func_list) == 1:
-                return arg
-            return helper(arg, func_list[1:])
+            if not funcs2:
+                ret = funcs[0](arg)
+
+                # This made an exception above
+                return helper(ret, funcs[1:])
+
+                # hot_fix
+                # funcs2 = funcs
+
+            else:
+                funcs2 = funcs2[0]
+                ret = funcs2[0](arg)
+
+            if len(funcs2) == 1:
+                return ret
+
+            return helper(ret, funcs2[1:])
+
         return helper
 
 
-fun = Pipeline.pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
+# 2) UnboundLocalError: local variable 'funcs' referenced before assignment
+# class Pipeline:
+#     @staticmethod
+#     def pipeline(*funcs):
+#
+#         def helper(arg, *args):
+#
+#             ret = funcs[0](arg)
+#             funcs = funcs[1:]
+#
+#             return helper(ret)
+#         return helper
+
+
+# 3) Tests: 3 pass / 0 fail
+class Pipeline:
+    @staticmethod
+    def pipeline(*funcs):
+        def helper(arg, *args):
+
+            if not args:
+                funcs2 = funcs
+            else:
+                funcs2 = args[0]
+
+            ret = funcs2[0](arg)
+
+            if len(funcs2) == 1:
+                return ret
+
+            return helper(ret, funcs2[1:])
+        return helper
+
+
+fun = Pipeline.pipeline(lambda x: x / 2)
 print(fun(3))  # should print 5.0
