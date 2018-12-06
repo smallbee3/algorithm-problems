cf96b163c2a323c59bb05f5b2fbb072362be38f3
smallbee <smallbee3@gmail.com>
2018-12-06 16:43:43 +0900

    [python/codesignal] comments modified

--- python/codesignal/3_checkPalindrome.py
+++ python/codesignal/3_checkPalindrome.py
@@ -1,10 +1,32 @@
 """
+3_checkPalindrome
+
+Given the string, check if it is a palindrome.
+
+
+Example
+
+Given an array of integers, find the pair of adjacent elements
+that has the largest product and return that product.
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+
+"""
+"""
 181205
 
 Subject - Palindrome
 
 Learning
-    1. compare string with reverse index
+    1. Compare string with reverse index
+    2. The usage of 'reversed' method (but, reverse index is good enough
 
 """
 
