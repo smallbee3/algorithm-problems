fb847821e66d7161ddb41f47ffc7e50a99da5660
Clark <clark@Clarks-MacBook-Pro.local>
2019-02-05 10:58:13 +0900

    [sql/testdome] 08_test_1 added

---
+++ sql/testdome/08_test_1.sql
@@ -0,0 +1,37 @@
+/*
+Question 1 of 4
+
+Which of the following statements are true for the database index?
+(multiple correct answers possible)
+*/
+
+-- Submitted
+ ㅁ It causes performance degredation of the INSERT operation
+
+ ㅁ It require additional storage space
+
+ ㅁ It always improves the speed of the UPDATE operation
+
+vㅁ It is very useful on data with low cardinality/uniqueness
+
+vㅁ It can improve the speed of the SELECT operation
+
+
+
+-- Answer
+vㅁ It causes performance degredation of the INSERT operation
+
+vㅁ It require additional storage space
+
+ ㅁ It always improves the speed of the UPDATE operation
+
+ ㅁ It is very useful on data with low cardinality/uniqueness
+
+vㅁ It can improve the speed of the SELECT operation
+
+
+
+https://lalwr.blogspot.com/2016/02/db-index.html
+
+http://choko11.tistory.com/entry/인덱스-1-개념종류주의사항
+
