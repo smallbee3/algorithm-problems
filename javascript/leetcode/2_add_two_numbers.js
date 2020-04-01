3e2d13049cf2735d250d40710e768f073fc9f450
smallbee <smallbee3@gmail.com>
2020-04-02 00:58:41 +0900

    [js/leetcode] Retry '2_add_two_numbers'

--- javascript/leetcode/2_add_two_numbers.js
+++ javascript/leetcode/2_add_two_numbers.js
@@ -120,3 +120,114 @@ printLinkedList(result)
 // printLinkedList(makeLinkedList(465))
 // result = addTwoNumbers(makeLinkedList(1000000000000000000000000000001), makeLinkedList(465))
 // printLinkedList(result)
+
+
+
+
+// 200401
+// 3) (retrial)
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+function printLinkedList(l) {
+    let numStr = ''
+    while(true) {
+        numStr += l.val
+        if (!l.next) break;
+        numStr += ' -> '
+        l = l.next
+    }
+    console.log('(', numStr, ')')
+}
+
+function makeLinkedList(value) {
+    let prevNode = null;
+    let node = null;
+    for (var i of value.toString()) {
+        node = new ListNode(Number(i))
+        if (prevNode) {
+            node.next = prevNode;
+        }
+        prevNode = node;
+    }
+    return node;
+}
+
+// ----------------------------------------------------
+
+function makeLinkedListFromArr(arr) {
+    let prev;
+    for(let i=arr.length-1; i >= 0; i--) {
+        node = new ListNode(arr[i]);
+        if (prev) node.next = prev;
+        prev = node;
+    }
+    return node;
+}
+
+function arraySum(arr1, arr2) {
+    let sumArr = [];
+    [arrMain, arrSub] = arr1.length > arr2.length ? [arr1, arr2] : [arr2, arr1]
+
+    for (let i = 0; i < arrMain.length; i++) {
+        // arrMain + arrSub
+        if (i < arrSub.length) {
+            sumArr.push(arrMain[i] + arrSub[i]);
+        }
+        // arrMain
+        else {
+            sumArr.push(arrMain[i]);
+        }
+    }
+    console.log('before : ', sumArr);
+
+    for (let i = 0; i < sumArr.length; i++) {
+        if (sumArr[i] > 9) {
+            quotient = Math.floor(sumArr[i] / 10);
+            remainder = sumArr[i] % 10;
+
+            sumArr[i] = remainder;
+            // [10] -> [0, 1]
+            if (typeof(sumArr[i+1]) === 'number') {
+                sumArr[i+1] += quotient;
+            }
+            else {
+                sumArr.push(quotient);
+            }
+        }
+    }
+    console.log('after  : ', sumArr);
+
+    return sumArr
+}
+
+function makeNumArr(l) {
+    let numArr = [];
+    while(true) {
+        numArr.push(l.val);
+        if (!l.next) break;
+        l = l.next;
+    }
+    return numArr;
+}
+
+var addTwoNumbers = function(l1, l2) {
+    sumArr = arraySum(makeNumArr(l1), makeNumArr(l2));
+    l = makeLinkedListFromArr(sumArr);
+    return l;
+};
+
+printLinkedList(makeLinkedList(342));
+printLinkedList(makeLinkedList(465));
+result = addTwoNumbers(makeLinkedList(342), makeLinkedList(465));
+printLinkedList(result);
+
+
+
+// printLinkedList(makeLinkedList(10000000000000000001))
+// printLinkedList(makeLinkedList(465))
+// result = addTwoNumbers(makeLinkedList(1000000000000000000000000000001), makeLinkedList(465))
+// printLinkedList(result)
