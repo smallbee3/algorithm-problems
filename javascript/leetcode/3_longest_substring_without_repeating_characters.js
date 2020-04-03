5c06685cae38c3f83ee044413ce2477394867efd
smallbee <smallbee3@gmail.com>
2020-04-04 01:37:46 +0900

    [js/leetcode] Review '3_longest_substring_without_repeating_characters'

--- javascript/leetcode/3_longest_substring_without_repeating_characters.js
+++ javascript/leetcode/3_longest_substring_without_repeating_characters.js
@@ -1,7 +1,11 @@
-/**
- * @param {string} s
- * @return {number}
- */
+
+// 1) hashmap
+// 600 ms	87 MB
+
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
 var lengthOfLongestSubstring = function(s) {    
     function findLength(value) {
         let count = 0;
@@ -31,3 +35,35 @@ result = lengthOfLongestSubstring("abcabcbb")
 // result = lengthOfLongestSubstring("bbbbb")
 // result = lengthOfLongestSubstring("pwwkew")
 console.log(result)
+
+
+
+
+// 2) Simpler than 1)
+// 496 ms	41.9 MB
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let longest = 0;
+    for (let i = 0; i < s.length; i++) {
+        let store = [];
+        for (const a of s.slice(i)) {
+            if (store.indexOf(a) < 0) {
+                store.push(a);
+            }
+            else {
+                break;
+            }
+        }
+        longest = longest > store.length ? longest : store.length;
+    }
+    console.log(longest);
+    return longest;
+};
+
+lengthOfLongestSubstring('abcabcbb');
+lengthOfLongestSubstring('bbbbb');
+lengthOfLongestSubstring('pwwkew');
