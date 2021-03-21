335567d8eb19f852d96a3b884300d3b5c2a34bc9
smallbee3 <smallbee3@gmail.com>
2021-03-21 16:49:33 +0900

    Add eslint and prettier config

---
+++ javascript/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // eslint-plugin-prettier + eslint-config-prettier
+    'prettier/@typescript-eslint', // Deactivate conflicting rules of @typescript-eslint/eslint-plugin
+    'plugin:react/recommended',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'simple-import-sort', // eslint-plugin-simple-import-sort
+  ],
+  rules: {
+    semi: [2, 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'always'],
+    'no-new-object': 'error',
+    'no-array-constructor': 'error',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-useless-escape': 'warn',
+    // third-party
+    'simple-import-sort/imports': 'error',
+    // typescript
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-var-requires': 'warn',
+    // react
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
